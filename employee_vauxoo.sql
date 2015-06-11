68ab20e53687693561db0e8bddf4671c0d812694
rblugo <rblugo81@gmail.com>
2015-06-11 15:07:33 -0500

    agregar hobbies

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -1,8 +1,10 @@
 sudo -u postgres createdb employee_employee
 
-CREATE TABLE employee (id int(8) primary key, first_name(30) varchar, last_name varchar(30, department varchar(20) references employee_department ));
+CREATE TABLE employee (id int(8) primary key, first_name(30) varchar, last_name varchar(30, department varchar(20) references employee_department, hobby varchar(20) references employee_hobby, hobby2 varchar(20) references employee_hobby);
 
 CREATE TABLE employee_department (id int(8), name varchar(20) primary key, description varchar(350));
+
+CREATE TABLE employee_hobby (id int(8), name varchar(20) primary key, description varchar(350));
 
 INSERT INTO employee VALUES ('92938', 'Ricardo', 'Bustillos Lugo');
 INSERT INTO employee VALUES ('34343', 'Jorge Alejandro', 'de la Torre Pichardo');
@@ -15,3 +17,7 @@ INSERT INTO employee_department VALUES ('84483', 'Finanzas', 'encargados de la c
 INSERT INTO employee_department VALUES ('84787', 'Administracion', 'Departamento que gestiona todos los recursos y procesa las solicitudes de la empresa');
 INSERT INTO employee_department VALUES ('87287', 'Almacen', 'Departamento encargado de administrar todos los productos y servicios fisicos que se encuentran en la empresa');
 INSERT INTO employee_department VALUES ('83784'. 'Informatica', 'Departamento encargado del sistema local de computo tanto como el equipo como su mantenimiento y configuracion');
+
+INSERT INTO employee_hobby VALUES ('8348', 'Leer', 'Gusto por la literatura de diferente genero');
+INSERT INTO employee_hobby VALUES ('6474', 'Football Americano', 'Deporte de alto rendimiento');
+INSERT INTO employee_hobby VALUES ('7484', 'Musica', 'Escuchar o Tocar algun instrumento y disfrutar de la 
