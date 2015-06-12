96f2c7217f6091ed3b4d1513f18f7ae838075244
rblugo <rblugo81@gmail.com>
2015-06-12 11:40:34 -0500

    primes

---
+++ primes.py
@@ -0,0 +1,14 @@
+# Ricardo Bustillos Lugo
+# 12 de junio del 2015
+# Version 1.0 
+#Clase que calcula si un numero es primo
+class prime_class():
+#Metdo que define si un numero es primo dividiendo entre 2 si es el resultado es diferente a 0
+#Regresara un True de lo contrario False
+
+	def is_prime (self, num)
+	
+		if num % 2 != 0
+	
+			return True
+		return False
