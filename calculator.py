384e2fe48726b982b4ec954069d2e7ba420baff3
rblugo <rblugo81@gmail.com>
2015-06-12 12:35:08 -0500

    calculator

---
+++ calculator.py
@@ -0,0 +1,14 @@
+# Ricardo Bustillos Lugo
+# 12 de junio del 2015
+# Version 1.0 
+#Clase para crear una calculadora
+class calculator_class():
+	
+milista = {1,2,3,4,5,6,7,8,9,10}
+#Metodo que manda llamar una lista ya creada y ira sumando de uno por uno los numeros que esten dentro de esa lista
+
+			def sum (self, milista)
+				sum = 0
+				for ( x in milista):
+					sum = sum + x
+				return sum
